548972202c012d7b36d5395e0a9dc2d708202281
NastyaZ23 <nastyh1233@gmail.com>
2023-05-23 12:16:52 +0300

    feat: dialogs style

--- src/components/Dialogs/index.tsx
+++ src/components/Dialogs/index.tsx
@@ -1,7 +1,37 @@
 import style from './Dialogs.module.scss';
 
+import userAvatar from 'assets/defaultUserIcon.svg';
 import { ReturnComponentType } from 'types';
 
 export const Dialogs = (): ReturnComponentType => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={style.dialogsWrapper}>
+      <Dialog />
+      <Dialog />
+      <Dialog />
+    </div>
+  );
+};
+
+const Dialog = (): ReturnComponentType => {
+  return (
+    <div className={style.dialogWrapper}>
+      <div className={style.avatarBlock}>
+        <div className={style.avatarWrapper}>
+          <img src={userAvatar} alt="user avatar" className={style.avatar} />
+        </div>
+      </div>
+      <div className={style.messageBlock}>
+        <div className={style.headerMessageBlock}>
+          <span className={style.userName}>765756757</span>
+          <span className={style.date}>22.04.1997</span>
+        </div>
+        <span className={style.messageWrapper}>
+          <span className={style.messageText}>
+            sdfsd dsffsdf eerre er werwer wer werew rewrew rwer werwer erew rwe rwsdfsfs
+          </span>
+        </span>
+      </div>
+    </div>
+  );
 };
