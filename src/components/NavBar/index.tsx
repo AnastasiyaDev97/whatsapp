02f8dc8c99ebd1527144b52cb2ecf13f70eb2127
NastyaZ23 <nastyh1233@gmail.com>
2023-05-23 11:37:00 +0300

    feat: header, navbar styles

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,10 +1,11 @@
+import style from './NavBar.module.scss';
+
 import { Dialogs, Header } from 'components';
 import { ReturnComponentType } from 'types';
 
 export const NavBar = (): ReturnComponentType => {
   return (
-    <aside>
-      NavBar
+    <aside className={style.navbarBlock}>
       <Header />
       <Dialogs />
     </aside>
